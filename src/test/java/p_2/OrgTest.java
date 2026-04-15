676503f357ce6625e124a008f4da2ccddc865817
Ravikiran Jakkannavar <ravikiran3169@gmail.com>
2026-04-15 16:22:39 +0530

    1St

---
+++ src/test/java/p_2/OrgTest.java
@@ -0,0 +1,16 @@
+package p_2;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+
+	@Test
+	public void createorgTest() {
+		System.out.println("execute createorgTest");
+		System.out.println("execute createorgTest");
+	}
+	@Test
+	public void modfiyorgTest() {
+		System.out.println("execute modifyorgTest");
+	}
+}
